087389df2fdd8ca143d4b3bad2805541941b2dbf
Machine256 <73296831+Machine256@users.noreply.github.com>
2020-10-22 16:15:13 +0200

    Create ImageProcessing.py

---
+++ ImageProcessing.py
@@ -0,0 +1,10 @@
+"""
+ABOUT:
+
+This module gets an image as an input and outputs a % value that represent how "round" the ball is. 
+"""
+
+import pygame
+
+def ImageProcessing(image):
+  return roundness
